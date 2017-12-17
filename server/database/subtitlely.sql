e727619b96f82a35fb778a995b601402728038d4
Anas Abudayah <anas.abudayah@gmail.com>
2017-12-17 21:55:32 +0200

    rename database

--- server/database/subtitlely.sql
+++ server/database/subtitlely.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 17, 2017 at 08:23 PM
+-- Generation Time: Dec 17, 2017 at 08:54 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.8
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `subtitely`
+-- Database: `subtitlely`
 --
 
 -- --------------------------------------------------------
