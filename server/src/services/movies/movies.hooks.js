f015490174464d22d5a3810f7142f4f2caa656a9
Anas Abudayah <anas.abudayah@gmail.com>
2017-12-18 07:52:02 +0200

    search query

--- server/src/services/movies/movies.hooks.js
+++ server/src/services/movies/movies.hooks.js
@@ -26,6 +26,29 @@ function includeParser() {
   };
 }
 
+// @REVIEW to search a query just write this
+// http://localhost:3030/movies?title[$like]=%The%
+// docs => https://docs.feathersjs.com/api/databases/querying.html
+//
+// function search () {
+//   return function (hook) {
+//     console.log(hook.params.query); // eslint-disable-line
+//     const model = hook.app.service('subtitles').Model;
+//     hook.params.query.q = hook.params.q;
+//     hook.params.sequelize = {
+//       title: {
+//         $like: hook.params.query.q
+//       },
+//       include: [
+//         {
+//           model: model,
+//           // attributes: ['title'],
+//         }
+//       ]
+//     };
+//   };
+// }
+
 module.exports = {
   before: {
     all: [],
